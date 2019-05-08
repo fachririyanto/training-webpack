c2bd29432d705679a9b492bd4087c2657ac2d624
Fachri Riyanto <fachririyanto@gmail.com>
2019-05-08 10:32:15 +0700

    Add example of basic configuration.

---
+++ basic-reactjs/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        filename: 'script.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
